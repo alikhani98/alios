c4a296409250c4337306b577cf62468fdaeba576
alikhani98 <alikhani98@yahoo.com>
2026-07-08 14:34:10 +0330

    chore: revert unnecessary config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import path from "node:path";
-import { fileURLToPath } from "node:url";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ mode }) => ({
   base: mode === "development" ? "/" : "/alios/",
