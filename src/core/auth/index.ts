62d44f9643bb3cdbdbf15e908424ba9f97d5127a
alikhani98 <alikhani98@users.noreply.github.com>
2026-07-27 14:16:09 +0330

    docs(auth): define local record association contract

--- src/core/auth/index.ts
+++ src/core/auth/index.ts
@@ -1,4 +1,5 @@
 export * from "./AuthRuntimeProvider";
 export * from "./LocalOnlyAuthProvider";
+export * from "./localRecordAssociationContract";
 export * from "./sessionLifecycleContract";
 export * from "./types";
